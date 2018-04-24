c64844cebac54c649d3a87f786425e43faeec59e
richard <ricsmarsi@gmail.com>
2018-04-24 17:03:58 -0300

    modificado o arquivo welcome acrescentando estrutura if

--- welcome.sh
+++ welcome.sh
@@ -3,3 +3,16 @@ clear;
 echo -n "Digite o seu nome: ";
 read nome;
 echo "Hello "$nome;
+echo -n "Digite a sua idade: ";
+read idade;
+clear;
+echo "-----------------------";
+echo "nome: "$nome;
+echo "idade: "$idade
+if [ $idade -lt 18 ]; then
+	echo "menor de idade";
+else
+	echo "maior de idade";
+fi
+echo "-----------------------";
+exit;
